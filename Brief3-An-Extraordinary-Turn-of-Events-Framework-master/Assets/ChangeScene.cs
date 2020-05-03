6a522e3ab074dd7fec0131b821d56e6a2a9208ba
WilliamAlexanderCraig <31279556+WilliamAlexanderCraig@users.noreply.github.com>
2020-05-04 05:40:19 +0800

    w

---
+++ Brief3-An-Extraordinary-Turn-of-Events-Framework-master/Assets/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public UnityEvent titleScene;
+    public UnityEvent mainScene;
+    
+    public void changeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+   
+}
--- Brief3-An-Extraordinary-Turn-of-Events-Framework-master/Assets/NotGoal.cs
+++ Brief3-An-Extraordinary-Turn-of-Events-Framework-master/Assets/NotGoal.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class NotGoal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform Ball;
+    public ChangeScene changeScene;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(Vector3.Distance(transform.position, Ball.position));
+        if (Vector3.Distance(transform.position, Ball.position) < 1)
+        {
+            Debug.Log("gotcha");
+            changeScene.mainScene.Invoke();
+        }
     }
 }
