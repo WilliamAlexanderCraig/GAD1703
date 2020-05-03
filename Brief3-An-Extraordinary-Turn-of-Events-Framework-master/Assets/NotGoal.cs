fb4539a4cf998280fb347f6bf6fa651008a2e0f6
WilliamAlexanderCraig <31279556+WilliamAlexanderCraig@users.noreply.github.com>
2020-05-04 04:26:06 +0800

    new

---
+++ Brief3-An-Extraordinary-Turn-of-Events-Framework-master/Assets/NotGoal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotGoal : MonoBehaviour
+{
+
+    public Transform Ball;
+    public float distance = 1;
+    public GameManager gm;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(Ball.position, transform.position) < distance)
+        {
+           
+        }
+    }
+}
---
+++ Brief3-An-Extraordinary-Turn-of-Events-Framework-master/Assets/PaddleController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PaddleController : MonoBehaviour
+{
+    public GameObject Paddle1;
+    public GameObject Paddle2;
+    public GameObject Paddle3;
+
+    public UnityEvent TogglePaddle1;
+    public UnityEvent TogglePaddle2;
+    public UnityEvent TogglePaddle3;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
